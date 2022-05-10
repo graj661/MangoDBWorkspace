8350e1edbe701517b8b881a860d9b618e8cde01f
unknown <graj661@gmail.com>
2022-05-10 21:57:49 +0530

    added small debug lines

--- src/main/java/com/mango/MangoDBWorkspace/MangoDbWorkspaceApplication.java
+++ src/main/java/com/mango/MangoDBWorkspace/MangoDbWorkspaceApplication.java
@@ -8,6 +8,7 @@ public class MangoDbWorkspaceApplication {
 
 //--main Application ui
 	public static void main(String[] args) {
+		System.out.println("111111111111111");
 		SpringApplication.run(MangoDbWorkspaceApplication.class, args);
 	}
 

8cbe61bd18fa5204b21acd97672f674be5f3707a
unknown <graj661@gmail.com>
2022-05-10 21:56:16 +0530

    added small debug lines

--- src/main/java/com/mango/MangoDBWorkspace/MangoDbWorkspaceApplication.java
+++ src/main/java/com/mango/MangoDBWorkspace/MangoDbWorkspaceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MangoDbWorkspaceApplication {
 
-//--main Application 
+//--main Application ui
 	public static void main(String[] args) {
 		SpringApplication.run(MangoDbWorkspaceApplication.class, args);
 	}

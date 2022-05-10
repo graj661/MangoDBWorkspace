9b532affb53f1c112a6a6793d39971223f548be0
unknown <graj661@gmail.com>
2022-05-10 21:48:08 +0530

    added project setup code

---
+++ src/main/java/com/mango/MangoDBWorkspace/MangoDbWorkspaceApplication.java
@@ -0,0 +1,13 @@
+package com.mango.MangoDBWorkspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangoDbWorkspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangoDbWorkspaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mango/MangoDBWorkspace/MangoDbWorkspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mango.MangoDBWorkspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangoDbWorkspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
